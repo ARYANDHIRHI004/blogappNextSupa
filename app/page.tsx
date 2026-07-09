1729616b4c97a2d382ce4b3b0cfddec146b38259
ARYANDHIRHI004 <aryandhirhi004@gmail.com>
2026-07-10 01:26:26 +0530

    initialize blog app with nextjs and supaabase

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+export default function Home() {
+  return (
+    <div className="bg-black h-screen flex justify-center items-center">
+        <Button variant={"secondary"}>Click Me</Button>
+    </div>
+  );
+}
