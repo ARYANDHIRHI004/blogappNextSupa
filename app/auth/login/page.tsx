d099fa450f2d713a883e30b31c9822d5257860ed
ARYANDHIRHI004 <aryandhirhi004@gmail.com>
2026-07-10 02:38:45 +0530

    feat: implement signup feature

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,68 @@
+"use client";
+import { Card } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import React, { useState } from "react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    console.log(email);
+    console.log(password);
+    setEmail("");
+    setPassword("");
+  };
+
+  return (
+    <main className="min-h-screen bg-zinc-950 flex items-center justify-center px-4">
+      <Card className={"w-full max-w-md p-8 bg-zinc-900 border-zinc-800"}>
+        <h1 className=" text-3xl font-bold text-emerald-400"> Login </h1>
+        <form onSubmit={handleSubmit} className="space-y-4 ">
+          <div>
+            <Label className="block text-sm font-medium text-zinc-300 mask-b-to-red-200">
+              Email
+            </Label>
+            <Input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="border-0 mt-2 h-10 text-white"
+              placeholder="Enter your email"
+              required
+            />
+          </div>
+          <div>
+            <Label className="block text-sm font-medium text-zinc-300 mask-b-to-red-200">
+              Password
+            </Label>
+            <Input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="border-0 mt-2 h-10 text-white "
+              placeholder="Enter Password"
+              required
+            />
+          </div>
+          {
+            error && <div className="p-3 bg-red-900/20 rounded-xl text-red-300 border border-red-800">{error}</div>
+          }
+          <Button className={"bg-emerald-400 hover:bg-emerald-500 w-full h-10 mt-2"} type="submit">Sign Up</Button>
+
+          
+          <p className="text-center text-zinc-400">
+            Don't have an account |<Link className="text-emerald-400" href={"/auth/signup"}> Sign Up</Link>
+          </p>
+
+        </form>
+      </Card>
+    </main>
+  );
+};
+
+export default LoginPage;
---
+++ app/auth/signup-success/page.tsx
@@ -0,0 +1,12 @@
+import { Card } from '@/components/ui/card'
+import React from 'react'
+
+const SignUpSuccess = () => {
+  return (
+    <main>
+      <Card></Card>
+    </main>
+  )
+}
+
+export default SignUpSuccess
